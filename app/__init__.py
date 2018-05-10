c8b1333157f6d07cc79989bbc0fe143c2cfeca32
brent-shaw <shaw@live.co.za>
2018-05-10 09:17:57 +0200

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -4,6 +4,6 @@ from flask_socketio import SocketIO
 print('Hit \'Ctrl+C\' when done playing with your phone!')
 
 app = Flask(__name__)
-socketio = SocketIO(app, logger=False, engineio_logger=False)
+socketio = SocketIO(app, logger=False, engineio_logger=False, threaded=True)
 
 from app import views
